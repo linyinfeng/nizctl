8fa0e889fcca265909458d427cf9df8c0a833f83
Lin Yinfeng <lin.yinfeng@outlook.com>
2026-09-25 15:43:42 +0800

    consts: add atom68 EC product id

--- src/consts.rs
+++ src/consts.rs
@@ -6,6 +6,7 @@ lazy_static::lazy_static! {
     pub static ref PRODUCT_ID: HashMap<u16, &'static str> = {
         let mut map = HashMap::new();
         map.insert(0x512a, "atom66");
+        map.insert(0x5132, "atom68");
         map.insert(0x5129, "micro84");
         map
     };
